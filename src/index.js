08e466385439f161212172d72daa6db3d043062b
kamalgo <kamal.252534@gmail.com>
2024-03-20 20:40:07 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function MyButton(){
+    return <button>click me</button>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <MyButton/>
+    <App />
+    <App />
     <App />
   </React.StrictMode>
 );
